9d5eb69f9632e8d727c544d78df498f0ea64a577
phyothukha <phyothukha2193@gmail.com>
2024-03-08 15:06:10 +0630

    fix: fix some error

--- client/src/store/server/comment/queries.ts
+++ client/src/store/server/comment/queries.ts
@@ -3,40 +3,13 @@ import { axios } from "..";
 import { Comment, SelectedComment, getCommentPayload } from "./interface";
 import { getParams } from "../../../utils/getParams";
 
-// const getComments = async ({
-//   id,
-//   ...payload
-// }: getCommentPayload): Promise<Comment> => {
-//   const params = getParams(payload);
-//   const res = await axios.get(`/get/comment/${id}?${params}`);
-//   return res.data;
-// };
-
-// export const useGetComment = ({ id, ...payload }: getCommentPayload) => {
-//   return useQuery({
-//     queryKey: ["get-comments", id, payload],
-//     queryFn: () => getComments({ id, ...payload }),
-//     select: (data): SelectedComment[] =>
-//       data.comments.map((comment) => ({
-//         id: comment?._id,
-//         user: comment.user,
-//         blog_id: comment.blog_id,
-//         blog_user_id: comment.blog_user_id,
-//         content: comment.content,
-//         createdAt: comment.createdAt,
-//         reply_user: comment.reply_user,
-//         replyCM: comment.replyCM,
-//         comment_root: comment?.comment_root,
-//       })),
-//   });
-// };
-
 const getComments = async ({
   id,
   ...payload
 }: getCommentPayload): Promise<Comment> => {
   const params = getParams(payload);
   const res = await axios.get(`/get/comment/${id}?${params}`);
+  console.log(res);
   return res.data;
 };
 
@@ -56,9 +29,5 @@ export const useGetComment = ({ id, ...payload }: getCommentPayload) => {
         createdAt: cmt.createdAt,
         comment_root: cmt.comment_root,
       })),
-
-    // select:(data):SelectedComment[]=>data.comments.map(()=>({
-
-    // }))
   });
 };
